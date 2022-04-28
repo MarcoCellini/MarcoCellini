6ad71f1c99a024f7f34170867dff2347caf87487
Marco-Cellini <marco.cellini@istitutoagnelli.it>
2022-04-28 13:36:28 +0200

    Update algoritmi_oradinamento

--- C/algoritmi_ordinamento/es2_cellini.c
+++ C/algoritmi_ordinamento/es2_cellini.c
@@ -3,10 +3,10 @@
 
 int cmpfunc (const void * a, const void * b)
 {
-   return ( *(int*)a - *(int*)b );
+   return ( *(char*)a - *(char*)b );
 }
 
-void print(int arr[])
+void print(char arr[])
 {
     for (int i = 9; i > 0; i--)
         printf ("%c ", arr[i]);
@@ -14,13 +14,12 @@ void print(int arr[])
 
 int main()
 {
-    char arr_in[10] = {'g', 'e', 'r', 'y', 'z', 'o', 'p', 'q', 'v', 'd'}; 
-    int arr_out[10];
+    char arr_in[10] = {'g', 'e', 'r', 'y', 'z', 'o', 'p', 'q', 'v', 'd'}, arr_out[10];
 
     for (int i = 0; i < 10; i++)
         arr_out[i] = arr_in[i];
 
-    qsort (arr_out, 10, sizeof (int), cmpfunc);
+    qsort (arr_out, 10, sizeof (char), cmpfunc);
 
     print (arr_out);
     printf ("\n\n");
