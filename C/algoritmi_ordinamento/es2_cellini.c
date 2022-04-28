38423e50c77e578c6c57c2933f048595ff0bb796
Marco-Cellini <marco.cellini@istitutoagnelli.it>
2022-04-28 13:28:42 +0200

    Update algoritmi_ordinamento/

---
+++ C/algoritmi_ordinamento/es2_cellini.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cmpfunc (const void * a, const void * b)
+{
+   return ( *(int*)a - *(int*)b );
+}
+
+void print(int arr[])
+{
+    for (int i = 9; i > 0; i--)
+        printf ("%c ", arr[i]);
+}
+
+int main()
+{
+    char arr_in[10] = {'g', 'e', 'r', 'y', 'z', 'o', 'p', 'q', 'v', 'd'}; 
+    int arr_out[10];
+
+    for (int i = 0; i < 10; i++)
+        arr_out[i] = arr_in[i];
+
+    qsort (arr_out, 10, sizeof (int), cmpfunc);
+
+    print (arr_out);
+    printf ("\n\n");
+
+    return 0;
+}
