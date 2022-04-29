beb7e141589b308f17ecefe6a8b74e3e60693cc4
Marco-Cellini <marco.cellini@istitutoagnelli.it>
2022-04-29 17:22:35 +0200

    add es6_cellini.c in C/algoritmi_ordinamento

--- C/algoritmi_ordinamento/es6_cellini.c
+++ C/algoritmi_ordinamento/es6_cellini.c
@@ -1,26 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
 
-int cmpfunc (const void * a, const void * b)
-{
+int cstring_cmp(const void *a, const void *b) 
+{ 
     const char **ia = (const char **)a;
     const char **ib = (const char **)b;
     return strcmp(*ia, *ib);
+} 
+
+void print(char **array, int l) 
+{ 
+    printf ("\n\n");
+    for(int i = 0; i < l; i++) 
+        printf("%s  ", array[i]);
+    printf ("\n\n");
 }
 
-int main()
+int main() 
 {
-    char arr_str[5] [8] = { {"ciao"},
-                            {"sono"},
-                            {"pippo"},
-                            {"pluto"},
-                            {"armadio"} };
+    char *strings[] = { "casa", "zoccolo", "terra", "centauro", "armadio", "pavimento" };
 
-    qsort (arr_str, 8, sizeof (char*), cmpfunc);
+    int l = sizeof(strings) / sizeof(char *);
 
-    for (int i = 0; i < sizeof(arr_str)/8; i++)
-        printf ("%s\t", arr_str[i]);
+    qsort(strings, l, sizeof(char *), cstring_cmp);
 
+    print(strings, l);
+    
     return 0;
-}
+} 
