73871a931084b02ddae16dfde5c18bfe9944df2e
Marco-Cellini <marco.cellini@istitutoagnelli.it>
2022-05-11 13:22:25 +0200

    cioa

--- C/verifica_11-05/es2_cellini/es2_cellini.c
+++ C/verifica_11-05/es2_cellini/es2_cellini.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -32,7 +31,6 @@ int main()
     for (c = getc(dettagli); c != EOF; c = getc(dettagli))
     {
         fscanf (dettagli, "%i\n", &n[i++]);
-        
     }
 
     fprintf (out, "Il treno delle %d e %d porta un ritardo di %d minuti. Chissà se mi rimborseranno i %d,%d euro del biglietto? Il giorno %d/%d/2007 ci sarà un’altra riunione alle %d.", n[5], n[3], n[6], n[2], n[4], n[1], n[3], n[0]);  
