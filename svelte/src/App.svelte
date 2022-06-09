157cc58ad4391252fa4cf008f4dadea57f80d663
Marco Cellini <marco.cellini@istitutoagnelli.it>
2022-06-09 12:22:03 +0200

    mod in svelte

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,61 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
-	<h1>CIAO {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Titolo</h1>
+	<nav>
+		<a href="javascript:window.open('https://www.google.com')" id="google" class="barra" title="Google">Google</a>
+		<a href="javascript:window.open('https://www.amazon.it')" id="amazon" class="barra" title="Amazon">Amazon</a>
+		<a href="javascript:window.open('https://www.spotify.com')" id="spotify" class="barra" title="Spotify">Spotify</a>
+		<a href="javascript:window.open('https://www.netflix.com')" id="netflix" class="barra" title="Netflix">Netflix</a>
+		<a href="javascript:window.open('https://www.poste.it')" id="poste" class="barra" title="Poste Italiane">Poste Italiane</a>
+	</nav>
 </main>
 
 <style>
-	main {
+	main{
+		background: linear-gradient(to left, gray, black);
+	}
+	h1{
+		font-size: 62px;
+		margin-top: auto;
 		text-align: center;
-		padding: 10em;
-		max-width: 240px;
-		margin: 0 auto;
+		font-weight: bold;
+		font-family: 'Times New Roman', Times, serif;
+		animation: colorchange 10s infinite;
 	}
-
-	h1 {
-		color: #00ff2a;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	@keyframes colorchange{
+	0% { color: rgb(183, 0, 255); }
+  	50% { color: rgb(0, 0, 255); }
+ 	100% { color: rgb(183, 0, 255); }
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	a{
+		text-decoration:none;
+	}
+	nav{
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 28px;
+		text-align: center;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+	}
+	#google{
+		color: rgb(255, 102, 0);
+	}
+	#amazon{
+		color: rgb(0, 238, 255);
+	}
+	#spotify{
+		color: rgb(2, 182, 2);
+	}
+	#netflix{
+		color: red;
+	}
+	#poste{
+		color: yellow;
+	}
+	.barra{
+		margin-right: 50px;
 	}
 </style>
