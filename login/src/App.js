433512ac82f8970e9546e512115d5e5a50bc3c45
Marco <Marco@softso.local>
2022-06-22 10:18:28 +0200

    adjust all

--- login/src/App.js
+++ login/src/App.js
@@ -1,32 +1,23 @@
-import './index.css';
-import Home from './pages/Home.js';
-import Login from './pages/Login.js';
-import Logout from './pages/Logout.js';
-import Myaccount from './pages/Myaccount.js';
-import Profile from './pages/Profile.js';
-import Register from './pages/Register.js';
-import Header from './component/Header.js';
-import Anagrafica from './pages/Anagrafica';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/myaccount" element={<Myaccount />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/anagrafica" element={<Anagrafica />} />
-        </Routes>
-      </Router>
-      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- login/src/index.js
+++ login/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-import Home from "./Home"
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container);           // createRoot(container!) if you use TypeScript
-root.render(<Home tab="home" />);
-
-//ReactDOM.render(<Home />, document.getElementById("root"));
-/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <h1>cia</h1>
-  <FormPropsTextFields />
-
+    <App />
   </React.StrictMode>
 );
 
@@ -23,4 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
