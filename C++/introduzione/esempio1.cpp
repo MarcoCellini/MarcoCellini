42fc3beeb21d4c8ff028f5bc2b6111d7548adaf0
Marco Cellini <marco.cellini@istitutoangnelli.it>
2022-09-21 12:36:09 +0200

    add in C++

---
+++ C++/introduzione/esempio1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+const float PI = 3.14159;
+
+class Cerchio {
+    float raggio;
+
+    public: 
+        void set_r(float r) {
+            raggio = r;
+        }
+        
+        float calcola_A() {
+            return(raggio * raggio * PI);
+        }
+
+        float calcola_P() {
+            return(2 * PI * raggio);
+        }
+};
+
+int main() {
+    Cerchio c1;
+
+    c1.set_r(3);
+
+    cout << "Area: " << c1.calcola_A() << endl << "Perimetro: " << c1.calcola_P() << endl;
+
+    system("PAUSE");
+
+    return 0;
+}
