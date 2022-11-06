49043f8fff761de44b4ca16a05c52cd53a8d35d5
MarcoCellini <marco.cellini@istitutoagnelli.it>
2022-11-06 10:52:28 +0100

    fix es3

--- C++/file/es_7-11/es3_cellini/main_cellini.cpp
+++ C++/file/es_7-11/es3_cellini/main_cellini.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cctype>
 
-char parola[] = "bomba";
+char parola[] = "BOMBA";
 
 using namespace std;
 
@@ -25,46 +25,55 @@ void trova() {
         
         for (int i = 0; i < 100; i++) {
             if (riga[i] == '\0') {break;}
-            else if (riga[i] == parola[0] || riga[i] == toupper(parola[0]) && ok[0] == 0) {
+            else if (riga[i] == parola[0] /* || riga[i] == toupper(parola[0]) */ && ok[0] == 0) {
                 posizioni[0] = i+1;
                 ok[0] = 1;
             }
-            else if(riga[i] == parola[1] || riga[i] == toupper(parola[1]) && ok[1] == 0) {
+            else if(riga[i] == parola[1] /* || riga[i] == toupper(parola[1]) */ && ok[1] == 0) {
                 if (ok[0] == 1) {
                     posizioni[1] = i+1;
                     ok[1] = 1;
                 }
             }
-            else if(riga[i] == parola[2] || riga[i] == toupper(parola[2]) && ok[2] == 0) {
+            else if(riga[i] == parola[2] /* || riga[i] == toupper(parola[2]) */ && ok[2] == 0) {
                 if (ok[1] == 1) {
                     posizioni[2] = i+1;
                     ok[2] = 1;
                 }
             }
-            else if(riga[i] == parola[3] || riga[i] == toupper(parola[3]) && ok[3] == 0) {
+            else if(riga[i] == parola[3] /* || riga[i] == toupper(parola[3]) */ && ok[3] == 0) {
                 if (ok[2] == 1) {
                     posizioni[3] = i+1;
                     ok[3] = 1;
                 }
             }
-            else if(riga[i] == parola[4] || riga[i] == toupper(parola[4]) && ok[4] == 0) {
+            else if(riga[i] == parola[4] /* || riga[i] == toupper(parola[4]) */ && ok[4] == 0) {
                 if (ok[3] == 1) {
                     posizioni[4] = i+1;
                     ok[4] = 1;
                 }   
             }
+            else if(riga[i] == parola[0] || riga[i] == parola[1] || riga[i] == parola[2] || riga[i] == parola[3] || riga[i] == parola[4]) {
+                if (ok[4] != 0) {
+                    riga[i] = 'X';
+                }
+            }
         }
 
-        for (int i = 0; i < 5; i++) {
-            cout << posizioni[i] << endl;
+        output << riga << "\t\t";
+        if (ok[4] != 0) {
+            output << "Questa e' una bomba\t\tPosizioni:\t";
+            for (int i = 0; i < 5; i++) {output << posizioni[i] << "\t";}
+            output << endl;
+        }
+        else {
+            output << "Questa NON e' una bomba" << endl;
         }
         
         for (int i = 0; i < sizeof(parola); i++) {
             ok[i] = 0;
             posizioni[i] = -1;
         }
-
-        cout << endl << endl;
     }
     
 
