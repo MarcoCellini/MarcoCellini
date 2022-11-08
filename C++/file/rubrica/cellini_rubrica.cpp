be82a156e913adddaffc65a856be29ce841fa0c5
MarcoCellini <marco.cellini@istitutoagnelli.it>
2022-11-08 18:32:22 +0100

    end rubrica in c++

--- C++/file/rubrica/cellini_rubrica.cpp
+++ C++/file/rubrica/cellini_rubrica.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
+#include <cstdlib>
 
 #define persone 3
-
 int old_contact = 0;
 
 using namespace std;
@@ -11,8 +12,8 @@ fstream Rubrica;
 
 class rubrica {
     public:
-        int numero;
-        string nome, cognome;
+        //int numero;
+        string nome, cognome, numero;
 
         rubrica() {}
         ~rubrica() {}
@@ -22,13 +23,19 @@ class rubrica {
 rubrica r[100];
 
 void search() {
+    string nome, cognome;
+    cout << "\nInserire il nome da cercare:\t";
+    cin >> nome;
+    cout << "Inserire il cognome da cercare:\t";
+    cin >> cognome;
 
-}
-
-void control(int n, int x) {
-    for (int i = 0; i < persone; i++) {
-        if (r[i].numero == n && i != x) {
-            r[i].numero = -1;
+    for (int i = 0; i < persone + old_contact; i++) {
+        if (nome == r[i].nome && cognome == r[i].cognome) {
+            cout << "\nUtente trovato\n" << r[i].nome << " " << r[i].cognome << ":\t" << r[i].numero << endl;
+            break;
+        }
+        else if (i == (persone + old_contact) - 1) {
+            cout << "\nUtente inesistente" << endl;
         }
     }
 }
@@ -36,37 +43,52 @@ void control(int n, int x) {
 void fill_from_file() {
     Rubrica.open("rubrica.txt", ios::in);
 
-    char line[100];
+    string line[3];
+    int i = 0;
     while (!Rubrica.eof()) {
-        Rubrica.getline(line, 100);
+        Rubrica >> line[i++];
+        Rubrica >> line[i++];
+        Rubrica >> line[i];
 
-        
+        r[old_contact].nome = line[0];
+        r[old_contact].cognome = line[1].substr(0, (line[1].length() - 1));
+        r[old_contact++].numero = line[2];
 
-        old_contact++;
+        i = 0;
     }
 
     Rubrica.close();
 }
 
+void control(string n, int x) {
+    for (int i = 0; i < x; i++) {
+        if (r[i].numero == n/*  && i != x */) {
+            r[x].numero = "null";
+            break;
+        }
+    }
+}
+
 void IO_fill_rub() {
     Rubrica.open("rubrica.txt", ios::app);
 
-    for (int i = 0; i < persone; i++) {
+    for (int i = old_contact; i < persone + old_contact; i++) {
         cout << "\nInserire il nome:\t";
         cin >> r[i].nome;
-        cout << "\nInserire il cognome:\t";
+        cout << "Inserire il cognome:\t";
         cin >> r[i].cognome;
-        cout << "\nInserire il numero di telefono:\t";
+        cout << "Inserire il numero di telefono:\t";
         cin >> r[i].numero;
         control(r[i].numero, i);
     }
 
-    for (int i = 0; i < persone; i++) {
-        if (r[i].numero != -1) {
-            Rubrica << r[i].nome << " " << r[i].cognome << ":\t" << r[i].numero << endl;
+    //Rubrica << endl;
+    for (int i = 0; i < persone + old_contact; i++) {
+        if (r[i].numero == "null") {
+            Rubrica << endl << r[i].nome << " " << r[i].cognome << ":\t" << "numero ripetuto";
         }
-        else {
-            Rubrica << r[i].nome << " " << r[i].cognome << ":\t" << "numero ripetuto" << endl;
+        else if (i >= old_contact) {
+            Rubrica << endl << r[i].nome << " " << r[i].cognome << ":\t" << r[i].numero;
         }
     }
 
@@ -76,6 +98,7 @@ void IO_fill_rub() {
 int main() {
     fill_from_file();
     IO_fill_rub();
+    search();
 
     return 0;
 }
