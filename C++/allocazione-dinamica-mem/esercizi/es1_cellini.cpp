d62e0bf1faf041a5f319f80141942ff07e0c4a93
MarcoCellini <marco.cellini@istitutoagnelli.it>
2022-12-23 09:56:52 +0100

    add fix es1 in esercizi C++

--- C++/allocazione-dinamica-mem/esercizi/es1_cellini.cpp
+++ C++/allocazione-dinamica-mem/esercizi/es1_cellini.cpp
@@ -38,34 +38,47 @@ void remove() {
 }
 
 void edit() {
-    
+    int x;
+    cout << "\nSelezionare la locazione da modificare:\t";
+    cin >> x; 
+    cout << "\nCambiare il nome da [" << alunni.at(--x).nome << "] a:\t";
+    cin >> alunni.at(x).nome;
+    cout << "\nCambiare il voto da [" << alunni.at(x).voto << "] a:\t";
+    cin >> alunni.at(x).voto;
 }
 
 void stampa() {
-    
+    cout << "Nome\t\tVoto\n-------------------------\n";
+    for (int i = 0; i < alunni.size(); i++) {
+        cout << alunni.at(i).nome << "\t\t" << alunni.at(i).voto << endl;
+    }
+    cout << endl;
 }
 
 int main() {
     firstAdd();
 
-    char x;
-    cout << "\nScegli la modalita':\n[i] per inserire\n[r] per rimuovere\n[m] per modificare\n........";
-    cin >> x;
-    switch (x) {
-        case 'i':
-            insert();
-        break;
+    for (int i = 0; i < 3; i++) {
+        char x;
+        cout << "\nScegli la modalita':\n[i] per inserire\n[r] per rimuovere\n[m] per modificare\n........";
+        cin >> x;
+        switch (x) {
+            case 'i':
+                insert();
+            break;
 
-        case 'r':
-            remove();
-        break;
+            case 'r':
+                remove();
+            break;
 
-        case 'm':
-            edit();
-        break;
+            case 'm':
+                edit();
+            break;
+        }
+
+        cout << "\nLista Finale:\n";
+        stampa();
     }
-
-    stampa();
-
+    
     return 0;
 }
