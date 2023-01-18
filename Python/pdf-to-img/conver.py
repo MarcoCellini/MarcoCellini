3c7d5c9995f691d312f1ef683de0913937b8d24a
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-01-18 09:37:41 +0100

    add feature in convert

--- Python/pdf-to-img/conver.py
+++ Python/pdf-to-img/conver.py
@@ -1,7 +1,12 @@
 import os
+import argparse
 from pdf2image import convert_from_path
 
-path = ""
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', help='name-file')
+args = parser.parse_args()
+
+path = "pdf/" + args.n
 name_dir = path[4 : -4]
 
 img = convert_from_path(path)
