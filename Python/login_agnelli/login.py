01d7ebecb9eb81716a9a4ec54a4c608d35f7ae68
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-01-25 21:22:04 +0100

    add feature in login_agnelli python

--- Python/login_agnelli/login.py
+++ Python/login_agnelli/login.py
@@ -1,16 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
+
+def close():
+    driver.close()
+
+    path = "geckodriver.log"
+    os.unlink(path)
 
 driver = webdriver.Firefox()        ## open firefox
 driver.get("https://google.com")                ## search the site
 
-name = driver.find_element(by = By.ID, value = 'ft_un')         ##insert user name
-name.send_keys('marco.cellini')
+try:
+    name = driver.find_element(by = By.ID, value = 'ft_un')         ##insert user name
+    name.send_keys('marco.cellini')
 
-psw = driver.find_element(by = By.ID, value = 'ft_pd')          ## insert password
-psw.send_keys('PippoPluto')
+    psw = driver.find_element(by = By.ID, value = 'ft_pd')          ## insert password
+    psw.send_keys('PippoPluto')
 
-psw.send_keys(Keys.ENTER)
+    psw.send_keys(Keys.ENTER)
 
-driver.close()
+    close()
+
+except:
+    print("Login già effettuato connessione ad internet già presente")
+    close()
