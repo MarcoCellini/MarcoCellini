fa871ae2260e4eb4cf042a30b69e6a58cf5180a4
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-02-17 09:26:47 +0100

    change dir in JS

--- JavaScript/introduction/index.js
+++
@@ -1,6 +0,0 @@
-console.log("hello I'm an indian guy");     /* per stampare delle informazioni nella console */
-
-window.alert("hello I'm an indian guy");    /* per creare un pop up nella pagina*/
-
-// si commenta cosi' o cosi' /* */      == C
-
