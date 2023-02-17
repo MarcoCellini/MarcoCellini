7892d30c0b93b555ea9467d4995a9f4df34efed1
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-02-17 09:27:53 +0100

    fix problem

--- JavaScript/login-test-selenium/login.js
+++ JavaScript/login-test-selenium/login.js
@@ -9,7 +9,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
   try {
     await driver.get('http://www.google.com');
     await driver.findElement(By.id('ft_un')).sendKeys('marco.cellini');
-    await driver.findElement(By.id('ft_pd')).sendKeys('CelliniM1234$', Key.RETURN);
+    await driver.findElement(By.id('ft_pd')).sendKeys('PippoPluto', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     await driver.quit();
