746d542545575c644ca3c484d96ec90ea21c9fbc
Marco Cellini <marco.cellini@istitutoangnelli.it>
2023-03-06 17:27:32 +0100

    add get_netsh

--- Python/get_netsh/get_netsh.py
+++ Python/get_netsh/get_netsh.py
@@ -3,7 +3,7 @@ import re
 
 command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
 
-profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+profile_names = (re.findall("Tutti i profili utente    : (.*)\r", command_output))
 
 wifi_list = []
 
@@ -11,17 +11,17 @@ if len(profile_names) != 0:
     for name in profile_names:
         wifi_profile = {}
 
-        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode('unicode_escape')
 
-        if re.search("Security key           : Absent", profile_info):
+        if re.search("Chiave di sicurezza           : Assente", profile_info):
             continue
         else:
 
             wifi_profile["ssid"] = name
 
-            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode('unicode_escape')
 
-            password = re.search("Key Content            : (.*)\r", profile_info_pass)
+            password = re.search("Contenuto chiave            : (.*)\r", profile_info_pass)
 
             if password == None:
                 wifi_profile["password"] = None
