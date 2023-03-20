b7b63dd7b859db4a8f8091a4fa185be4564da5f0
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-03-20 18:55:33 +0100

    end tris

--- tris_web/tris.js
+++ tris_web/tris.js
@@ -1,4 +1,4 @@
-let type = true;
+let type = true, cont = 0, win = false;
 
 let gioco = [
     [0, 0, 0],
@@ -52,55 +52,89 @@ function dp(r, c, n) {
     return false;
 }
 
-function victory(value) {
+function victory(value, type_win) {
+    win = true;
+    switch (type_win) {
+        case "dp":
+            document.getElementById("dp").style.visibility = "visible";
+            break;
+        case "dn":
+            document.getElementById("dn").style.visibility = "visible";
+            break;
+        case "h0":
+            document.getElementById("htop").style.visibility = "visible";
+            break;
+        case "h1":
+            document.getElementById("hmiddle").style.visibility = "visible";
+            break;
+        case "h2":
+            document.getElementById("hbottom").style.visibility = "visible";
+            break;
+        case "v0":
+            document.getElementById("vsx").style.visibility = "visible";
+            break;
+        case "v1":
+            document.getElementById("vm").style.visibility = "visible";
+            break;
+        case "v2":
+            document.getElementById("vdx").style.visibility = "visible";
+            break;
+    }
+
     if (value < 0) {
-        window.alert("Ha vinto il giocatore 1 [X]");
+        document.getElementById("pop-up").innerHTML = "Il vincitore è il giocatore 1 [X]";
     } else if (value > 0) {
-        window.alert("Ha vinto il giocatore 2 [O]");
+        document.getElementById("pop-up").innerHTML = "Il vincitore è il giocatore 2 [O]";
     }
 }
 
-function is_win(value) {
+function control() {
+    for (let r = 0; r < status.length; r++) {
+        for (let c = 0; c < status.length; c++) {
+            if (!status[r][c])
+                return false;
+        }   
+    }
+    return true;
+}
+
+function is_win(value, busy) {
     for (let i = 0; i < gioco.length; i++) {
         for (let j = 0; j < gioco.length; j++) {
             if (gioco[i][j] === value) {
                 if (dx(i, j, value)) {
-                    console.log("andiamo a destra");
                     if (dx(i, j + 1, value)) {
-                        console.log("win");
-                        victory(value);
+                        victory(value, "h" + i);
                     }
                 }
 
                 if (dw(i, j, value)) {
-                    console.log("andiamo giu");
                     if (dw(i + 1, j, value)) {
-                        console.log("win");
-                        victory(value);
+                        victory(value, "v" + j);
                     }
                 }
 
                 if (dn(i, j, value)) {
-                    console.log("andiamo diag neg");
                     if (dn(i + 1, j + 1, value)) {
-                        console.log("win");
-                        victory(value);
+                        victory(value, "dn");
                     }
                 }
 
                 if (dp(i, j, value)) {
-                    console.log("andiamo diag pos");
                     if (dp(i + 1, j - 1, value)) {
-                        console.log("win");
-                        victory(value);
+                        victory(value, "dp");
                     }
                 }
             }
         }
     }
+
+    if (busy == 9 && control() && !win)
+        document.getElementById("pop-up").innerHTML = "Parità";
 }
 
 function x(ID) {
+    cont += 1;
     if (type && status[ID[1]][ID[2]] == false) {
         document.getElementById(ID).innerHTML = "X";
         gioco[ID[1]][ID[2]] = -1;
@@ -112,7 +146,10 @@ function x(ID) {
         status[ID[1]][ID[2]] = true;
         type = true;
     }
-    console.log(gioco);
-    is_win(-1);
-    is_win(1);
+    is_win(-1, cont);
+    is_win(1, cont);
+}
+
+function restart() {
+    location.reload();
 }
