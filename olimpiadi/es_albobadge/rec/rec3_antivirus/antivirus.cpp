c93c3d190d77465e9b09af87ebc2c600d64e5d8a
Marco Cellini <marco.cellini@istitutoangnelli.it>
2023-05-09 13:05:51 +0200

    add in antivirus

--- olimpiadi/es_albobadge/rec/rec3_antivirus/antivirus.cpp
+++ olimpiadi/es_albobadge/rec/rec3_antivirus/antivirus.cpp
@@ -9,25 +9,28 @@ void solve(int t) {
     int N1, N2, N3, N4;
     cin >> N1 >> N2 >> N3 >> N4;
 
-    int M;
+    int M, pos = 0;
     cin >> M;
 
-    string F1, F2, F3, F4;
+    string F1, F2, F3, F4, tmp;
     cin >> F1 >> F2 >> F3 >> F4;
 
-    // scrivi in queste variabili la risposta
     int p1, p2, p3, p4;
 
-    // RISOLVI QUI IL PROBLEMA!!
+    while (pos != N1 - M) {
+        tmp = F1.substr(pos, M);
+        if ()
+        pos++;
+    }
+    
 
     cout << "Case #" << t << ": " << p1 << " " << p2 << " " << p3 << " " << p4 << endl;
 }
 
 int main() {
-    // le seguenti due righe possono essere utili per leggere e scrivere da file
 
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
     int T;
     cin >> T;
