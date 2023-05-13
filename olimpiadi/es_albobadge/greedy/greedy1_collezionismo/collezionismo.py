77c63c8b2ffac3efb409c711d6dbaa1c6abfce71
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-05-13 19:30:13 +0200

    add in olimpiadi

--- olimpiadi/es_albobadge/greedy/greedy1_collezionismo/collezionismo.py
+++ olimpiadi/es_albobadge/greedy/greedy1_collezionismo/collezionismo.py
@@ -1,7 +1,5 @@
 import sys
 
-sys.stdin = open('input.txt')
-sys.stdout = open('output.txt', 'w')
 
 def solve(t):
     input()
@@ -10,13 +8,21 @@ def solve(t):
     # N == statuine
     # K == scaffali
     C = list(map(int, input().strip().split()))
-
     C.sort()
 
-    
+    differenze = []
+    for i in range(N - 1):
+        differenze.append(C[i] - C[i + 1])
+    differenze.sort()
 
-    print(f"Case #{t}: {C}")
+    risultato = C[N - 1] - C[0]
+    for i in range(K - 1):
+        risultato += differenze[i]
 
+    print(f"Case #{t}: {risultato}")
+
+sys.stdin = open('collezionismo_input_2.txt')
+sys.stdout = open('output.txt', 'w')
 
 T = int(input().strip())
 
