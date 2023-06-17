6a6438555951d2caaf261e2a0440ab4cae03519f
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-06-17 13:45:11 +0200

    add in olimpiadi

--- olimpiadi/es_albobadge/grafi/grafi1_interruttori/interruttori.py
+++ olimpiadi/es_albobadge/grafi/grafi1_interruttori/interruttori.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open('input.txt')
+sys.stdin = open('interruttori_input_1.txt')
 sys.stdout = open('output.txt', 'w')
 
 def solve():
@@ -10,17 +10,37 @@ def solve():
     X = [None] * B
     Y = [None] * B
 
+    graph = [[] for _ in range(131072)]
+    distance = [None] * 131072
+    visited = [None] * 131072
+    queue = []
+
+    for i in range(N):
+        graph[i] = []
+        distance[i] = float('inf')
+        visited[i] = False
+
     for j in range(A):
         Z[j] = int(input())
+        distance[Z[j]] = 1
+        queue.append(Z[j])
+
     for j in range(B):
         X[j], Y[j] = map(int, input().split())
+        graph[X[j]].append(Y[j])
+        graph[Y[j]].append(X[j])
     
-    idx = 42 # memorizza qui l'indice della lampadina
-    num = A + B # memorizza qui il numero di interruttori
+    result = 0
+    while len(queue) > 0:
+        head = queue.pop(0)
+        if visited[head]: continue
+        visited[head] = True
+        result = head
+        for i in graph[head]:
+            distance[i] = min(distance[i], distance[head] + 1)
+            queue.append(i)
     
-    
-
-    return (idx, num)
+    return (result, distance[result])
 
 
 T = int(input())
